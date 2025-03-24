3d1444e9ca0e3bb0a9e4cd17e98af7c1f03c4bbf
fj-blanco <fjavib@gmail.com>
2025-03-24 18:06:22 +0100

    back to qkd-kem plugin

--- scripts/build_qkd_kem_provider.sh
+++ scripts/build_qkd_kem_provider.sh
@@ -7,6 +7,9 @@ mkdir -p /usr/local/lib/ossl-modules
 cd /qkd-kem-provider
 chmod +x scripts/fullbuild.sh
 
+export LIBOQS_BRANCH="0.12.0"
+export OQSPROV_CMAKE_PARAMS="-DQKD_KEY_ID_CH=ON"
+
 # Build the provider
 ./scripts/fullbuild.sh -F
 
